41ef43b3136694d20292902e3322502e9093aa61
joseb0rges <joseantoniosousaborges@gmail.com>
2023-09-03 02:10:58 +0000

    ajjuuhjhkhjk kjhkjh

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: 'api',
+      script: 'server.js', // O arquivo do seu servidor Node.js
+      watch: true, // Monitora alterações e reinicia o aplicativo
+    },
+  ],
+};
+
